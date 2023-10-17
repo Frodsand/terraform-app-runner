46eab1828af10b5fa1e9369e12e2b49d8f23fff1
Frodsand <fredrikrodsand@gmail.com>
2023-10-17 08:22:00 +0000

    prefix matches provider

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "pgr301-2021-terraform-state"
-    key    = "frro009lab8part2/apprunner-a-new-state.state"
+    key    = "frro/apprunner-a-new-state.state"
     region = "eu-north-1"
   }
 }
